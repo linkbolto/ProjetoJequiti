a68e3b6b1b7522c4e40d71469fed723f85726e55
linkbolto <linkbolto@gmail.com>
2020-06-20 21:33:33 -0300

    wip

--- front/assets/Script/Client/enterLobby.js
+++ front/assets/Script/Client/enterLobby.js
@@ -1,4 +1,4 @@
-import io from 'socket.io-client'
+import io from "socket.io-client";
 
 cc.Class({
   extends: cc.Component,
@@ -14,23 +14,24 @@ cc.Class({
   // update (dt) {},
 
   enterLobby() {
-    const socket = io("192.168.0.11:3500")
+    const socket = io("192.168.0.11:3500");
+    console.log("CLICK")
 
-    socket.on('connect', param => { 
-      console.log("CONECTADO")
+    socket.on("connect", (param) => {
+      console.log("CONECTADO");
 
-      console.log("param", param)
-    
-      socket.emit("joinLobby", {name: `player-${Math.round(Math.random()*100)}`}, () => {})
-      
-    })
+      console.log("param", param);
 
-    socket.on('message', (m) => {console.log(m)})
+      socket.emit(
+        "joinLobby", {name: 'lucas'},
+        (resp) => {
+          console.log("resp", resp);
+        }
+      );
+    });
 
-    socket.on('bla', m => console.log(m))
-
-    setTimeout(() => {
-      console.log("emitindo lobby...")
-    }, 500)
+    socket.on("message", (m) => {
+      console.log(m);
+    });
   },
 });
