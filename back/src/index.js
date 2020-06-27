f1500c433ffba16f484af912e2b183f0abfece04
lucas <linkbolto@gmail.com>
2020-06-27 16:07:09 -0300

    back

--- back/src/index.js
+++ back/src/index.js
@@ -1,26 +1,47 @@
 import socketio from 'socket.io'
-import Match from './game/match.js'
 import Lobby from './game/lobby.js'
+import {question1, question2} from './mocks/question.js'
+//SERVER
+
 
 const io = socketio(3500)
 
 const players = {}
 
-new Match().generateRounds()
+const state = {
+  currentQuestion: {}
+}
 
 io.on('connection', socket => {
   console.log('client connectado')
-  socket.emit("bla", "testando eventos")
+
+//  socket.emit("bla", "testando eventos")
 
   socket.on('joinLobby', (user, resp) => {
-    if (Lobby.join(user)) {
-      resp(true)
+    //if (Lobby.join(user)) {
+    resp(true)
 
+    setTimeout(() => {
+      state.currentQuestion = question1
+      socket.emit('roundStart', question1)
+    }, 2000)
+
+    setTimeout(() => {
+      state.currentQuestion = question2
+      socket.emit('roundStart', question2)
+    }, 10000)
+
+    socket.on("chooseResponse", (param, resp) => {
+      resp(state.currentQuestion.respostacerta)
+    })
+/*
       if(Lobby.isFull()) {
         io.sockets.emit("message", 'comecou!!!')
       }
     }
+*/
   })
+
 });
 
 export default io
---
+++ back/src/mocks/question.js
@@ -0,0 +1,17 @@
+export const question1 = {
+"pergunta": "EM QUE ESTADO DO BRASIL NASCEU A APRESENTADORA XUXA?",
+"resposta1": "RIO DE JANEIRO",
+"resposta2": "RIO GRANDE DO SUL",
+"resposta3": "SANTA CATARINA",
+"resposta4": "GOIÁS",
+"respostacerta": 2
+}
+
+export const question2 = {
+  "pergunta": "QUAL GRUPO DE PAGODE DO CANTOR CHRIGOR?",
+  "resposta1":"NEGRITUDE JR.",
+  "resposta2":"EXALTASAMBA",
+  "resposta3":"MOLEJO",
+  "resposta4":"SOWETO",
+  "respostacerta":1
+}
