9252f02c057faca72a9c71a91feb99f14ed9e294
linkbolto <linkbolto@gmail.com>
2020-07-07 21:38:13 -0300

    identacao

--- front/assets/Script/HomeScreen/PopulateHome.js
+++ front/assets/Script/HomeScreen/PopulateHome.js
@@ -1,33 +1,21 @@
-import {state} from '../Client/SocketClient'
+import { state } from "../Client/SocketClient";
 
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
+  properties: {
+    labelPlayerName: cc.Label,
+    labelPlayerCoins: cc.Label,
+    labelPowerUp1: cc.Label,
+    labelPowerUp2: cc.Label,
+    labelPowerUp3: cc.Label,
+  },
 
-        labelPlayerName : cc.Label,
-        labelPlayerCoins: cc.Label,
-        labelPowerUp1 : cc.Label,
-        labelPowerUp2 : cc.Label,
-        labelPowerUp3 : cc.Label,
-
-
-
-
-    },
-
-    start () {
-        console.log("state:", state)
-
-    this.labelPlayerName.string = state.player.name,
-    this.labelPlayerCoins.string = state.player.coins,
-    this.labelPowerUp1.string = state.player.powerup1,
-    this.labelPowerUp2.string = state.player.powerup2,
-    this.labelPowerUp3.string = state.player.powerup3
-
-
-
-    },
-
-    // update (dt) {},
+  start() {
+      this.labelPlayerName.string = state.player.name
+      this.labelPlayerCoins.string = state.player.coins;
+      this.labelPowerUp1.string = state.player.powerup1
+      this.labelPowerUp2.string = state.player.powerup2
+      this.labelPowerUp3.string = state.player.powerup3
+  },
 });
